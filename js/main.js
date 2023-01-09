52c985da02b3d9b3fa9166b145682339a5a2354d
Visay Vang <vsvang@gmail.com>
2023-01-09 10:30:26 -0500

    1/9/23, second commit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 import { tweetsData } from "./data.js";
-
-const tweetBtn = document.getElementById("tweet-btn");
-const tweetInput = document.getElementById("tweet-input");
-
-tweetBtn.addEventListener("click", () => {
-  console.log(tweetInput.value);
-});
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
 //clicking on specific card
 document.addEventListener("click", (e) => {
   if (e.target.dataset.like) {
     handleLikeClick(e.target.dataset.like);
+  } else if (e.target.dataset.retweet) {
+    handleRetweetClick(e.target.dataset.retweet);
+  } else if (e.target.dataset.reply) {
+    handleReplyClick(e.target.dataset.reply);
+  } else if (e.target.id === "tweet-btn") {
+    handleTweetClick();
   }
 });
 //passing down e.target.dataset.like as tweetId
@@ -18,13 +18,85 @@ function handleLikeClick(tweetId) {
   const targetTweetObj = tweetsData.filter((tweet) => {
     return tweet.uuid === tweetId;
   })[0];
-  targetTweetObj.likes++;
-  console.log(targetTweetObj);
+  //increment and decrement likes icon
+  if (targetTweetObj.isLiked) {
+    targetTweetObj.likes--;
+  } else {
+    targetTweetObj.likes++;
+  }
+  targetTweetObj.isLiked = !targetTweetObj.isLiked;
+  render();
 }
+
+//handle Retweet Click Function
+function handleRetweetClick(tweetID) {
+  const targetTweetObj = tweetsData.filter((tweet) => {
+    return tweet.uuid === tweetID;
+  })[0];
+  if (targetTweetObj.isRetweeted) {
+    targetTweetObj.retweets--;
+  } else {
+    targetTweetObj.retweets++;
+  }
+  targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted;
+  render();
+}
+
+//reply function
+function handleReplyClick(replyID) {
+  document.getElementById(`replies-${replyID}`).classList.toggle("hidden");
+}
+//tweet function
+function handleTweetClick(tweetID) {
+  const tweetInput = document.getElementById("tweet-input");
+  if (tweetInput.value) {
+    tweetsData.unshift({
+      handle: `@Scrimba 💎`,
+      profilePic: `images/troll.jpg`,
+      likes: 0,
+      retweets: 0,
+      tweetText: tweetInput.value,
+      replies: [],
+      isLiked: false,
+      isRetweeted: false,
+      uuid: uuidv4(),
+    });
+    render();
+  }
+  tweetInput.value = "";
+}
+
 //render to HTML
 function getFeedHtml() {
   let feedHtml = ``;
   tweetsData.forEach((tweet) => {
+    //add color to the heart icon
+    let likeIconClass = "";
+    if (tweet.isLiked) {
+      likeIconClass = "liked";
+    }
+    //add color to retweet icon
+    let retweetIconClass = "";
+    if (tweet.isRetweeted) {
+      retweetIconClass = "retweeted";
+    }
+    let repliesHtml = "";
+    if (tweet.replies.length > 0) {
+      tweet.replies.forEach((reply) => {
+        repliesHtml = `
+        <div class="tweet-reply">
+    <div class="tweet-inner">
+        <img src="${reply.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${reply.handle}</p>
+                <p class="tweet-text">${reply.tweetText}</p>
+            </div>
+        </div>
+</div>
+        `;
+      });
+    }
+
     feedHtml += `
      <div class="tweet">
     <div class="tweet-inner">
@@ -38,15 +110,18 @@ function getFeedHtml() {
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
-                <i class="fa-regular fa-heart" data-like="${tweet.uuid}"></i>
+                <i class="fa-regular fa-heart ${likeIconClass}" data-like="${tweet.uuid}"></i>
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-                <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
+                <i class="fa-solid fa-retweet ${retweetIconClass}" data-retweet="${tweet.uuid}"></i>
                     ${tweet.retweets}
                 </span>
             </div>   
         </div>            
+    </div>
+    <div class="hidden" id="replies-${tweet.uuid}">
+    ${repliesHtml}
     </div>
 </div>
     `;
